595ff6763a5ef193b28b8a2987c8d537b1dce816
Hussein Babu <husseinsalim419@gmail.com>
2023-11-06 13:19:19 +0300

    Styling Pages

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLogInView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLogInView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
 urlpatterns= [
     path('login/', CustomLogInView.as_view(), name='login' ),
+    path('logout/', LogoutView.as_view(next_page='login' ), name='logout' ),
+    path('register/', RegisterPage.as_view(), name='register' ),
     path('', TaskList.as_view(), name='tasks' ),
     path('task/<int:pk>', TaskDetail.as_view(), name='task' ),
     path('create-task/', TaskCreate.as_view(), name='task-create' ),
